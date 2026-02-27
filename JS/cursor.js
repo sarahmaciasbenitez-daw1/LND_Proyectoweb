35e5687497040eefd6fff35e6bfa52978974c3b2
Sarah Macías <sarahmaciasbenitez@alumno.ieselrincon.es>
2026-02-27 18:42:19 +0000

    Añadido visualización del cursor, arreglo de responsive en mediafisica

--- JS/cursor.js
+++ JS/cursor.js
@@ -1,5 +1,8 @@
 const character = document.getElementById("cursor-character");
+const toggleBtn = document.getElementById("toggle-cursor");
+const cursorIcon = document.getElementById("cursor-icon");
 
+let isCursorActive = true; 
 let mouseX = 0;
 let mouseY = 0;
 let charX = 0;
@@ -11,13 +14,34 @@ document.addEventListener("mousemove", (e) => {
 });
 
 function animate() {
-    charX += (mouseX - charX) * 0.1;
-    charY += (mouseY - charY) * 0.1;
+    /*para que se mueva el cursor si está en on*/
+    if (isCursorActive) {
+        charX += (mouseX - charX) * 0.1;
+        charY += (mouseY - charY) * 0.1;
 
-    character.style.left = charX + "px";
-    character.style.top = charY + "px";
+        character.style.left = charX + "px";
+        character.style.top = charY + "px";
+    }
 
     requestAnimationFrame(animate);
+}
+
+if (toggleBtn) { 
+    toggleBtn.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        
+        isCursorActive = !isCursorActive; 
+        /*se alterna la visibilidad del cursor*/
+        character.classList.toggle("cursor-hidden");
+
+        if (isCursorActive) {
+            cursorIcon.classList.replace("fa-eye-slash", "fa-eye");
+            cursorIcon.classList.remove("icon-disabled");
+        } else {
+            cursorIcon.classList.replace("fa-eye", "fa-eye-slash");
+            cursorIcon.classList.add("icon-disabled");
+        }
+    });
 }
 
 animate();
